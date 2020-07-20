b0cfde87a6be60e60a2fea99f5abb7b5cfc438aa
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-07-20 19:04:28 -0300

    Fix result findAll

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 
-
 const TransactionModel = require('../models/TransactionModel');
 
 const findAll = async (req, res) => {
-  
   let documents;
   await TransactionModel.find((err, res) => {
-   documents = JSON.stringify(res)
-  })
-  res.send(documents)
-}
+    const lenght = res.length;
+    const result = { lenght, transaction: res };
 
-module.exports = { findAll }
+    documents = JSON.stringify(result);
+  });
+
+  res.send(documents);
+};
+
+module.exports = { findAll };
